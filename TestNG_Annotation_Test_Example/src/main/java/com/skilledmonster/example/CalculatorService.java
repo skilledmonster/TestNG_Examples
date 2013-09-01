d6428e2fd7d4e82d0a2086c986f01e544177ff13
Jagadeesh <admin@skilledmonster.com>
2013-09-01 16:10:24 -0400

    adding data provider example

---
+++ TestNG_Annotation_Test_Example/src/main/java/com/skilledmonster/example/CalculatorService.java
@@ -0,0 +1,13 @@
+package com.skilledmonster.example;
+
+public interface CalculatorService {
+
+	int sum(int a, int b);
+
+	int multiple(int a, int b);
+
+	int div(int a, int b);
+
+	int sub(int a, int b);
+
+}
---
+++ TestNG_Annotation_Test_Example/src/test/java/com/skilledmonster/example/TestNGAnnotationTestDataProviderExample.java
@@ -0,0 +1,52 @@
+package com.skilledmonster.example;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * Example to demonstrate use of @Test annotation of TestNG framework
+ * 
+ * @author Jagadeesh Motamarri
+ * @version 1.0
+ */
+public class TestNGAnnotationTestDataProviderExample {
+
+	public CalculatorService service;
+
+	@BeforeClass
+	public void init() {
+		System.out.println("@BeforeClass: The annotated method will be run before the first test method in the current class is invoked.");
+		System.out.println("init service");
+		service = new SimpleCalculator();
+	}
+
+	@Test
+	public void testSum() {
+		System.out.println("@Test : testSum()");
+		int result = service.sum(10, 5);
+		Assert.assertEquals(result, 15);
+	}
+
+	@Test
+	public void testDiv() {
+		System.out.println("@Test : testDiv()");
+		int result = service.div(10, 5);
+		Assert.assertEquals(result, 2);
+	}
+
+	@Test
+	public void testMultiple() {
+		System.out.println("@Test : testMultiple()");
+		int result = service.multiple(10, 5);
+		Assert.assertEquals(result, 50);
+	}
+
+	@Test
+	public void testSub() {
+		System.out.println("@Test : testSub()");
+		int result = service.sub(10, 5);
+		Assert.assertEquals(result, 5);
+	}
+
+}
